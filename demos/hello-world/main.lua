1a478448160ea51ffdf2c6f05cc0d66590ade3e3
radwan92 <filip.radwaniecki@gmail.com>
2025-01-18 20:35:35 +0100

    demo: hello-world

---
+++ demos/hello-world/main.lua
@@ -0,0 +1,36 @@
+-- title:       hello, world!
+-- description: simple program to print scrolling text
+
+Text = {
+    x = 0,
+    y = 60,
+    color = 7,
+    text = "hello, world!",
+    len = print("hello, world!"),
+
+    update = function(self)
+        self.x += 1
+
+        if self.x > 127 then
+            self.x = 0
+        end
+    end,
+
+    draw = function(self)
+        print(self.text, self.x, self.y, self.color)
+
+        if self.x + self.len > 127 then
+            print(self.text, self.x - 128, self.y, self.color)
+        end
+    end
+}
+
+function _update60()
+    Text:update()
+end
+
+function _draw()
+    cls()
+
+    Text:draw()
+end

99ef74dca0927215e7d0c1c5e1090076be3032bb
radwan92 <filip.radwaniecki@gmail.com>
2025-01-20 21:01:29 +0100

    lib: colors

---
+++ libs/radlib/colors.lua
@@ -0,0 +1,19 @@
+--- @enum COLORS
+COLORS = {
+    black       = 0,
+    dark_blue   = 1,
+    dark_purple = 2,
+    dark_green  = 3,
+    brown       = 4,
+    dark_gray   = 5,
+    light_gray  = 6,
+    white       = 7,
+    red         = 8,
+    orange      = 9,
+    yellow      = 10,
+    green       = 11,
+    blue        = 12,
+    indigo      = 13,
+    pink        = 14,
+    peach       = 15
+}
